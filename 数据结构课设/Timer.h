58ba609ed9863ecc0de415d12b37866c92853d14
lanqiaoZzz <86559344+lanqiaoZzz@users.noreply.github.com>
2022-05-19 22:18:05 +0800

    Update Timer.h

--- 数据结构课设/Timer.h
+++ 数据结构课设/Timer.h
@@ -3,19 +3,23 @@
 #include<thread>
 using namespace std;
 
+class TimeTable;
 
 class Timer {
 
 public:
 
-	//模拟系统时间 + 闹钟提醒 + 活动过期后更新排序结果
-	//static void task();
-
 	//开始计时
-	//static void start();
+	static void start(TimeTable* table);
 
 	//停止计时
-	//static void stop();
+	static void stop();
+		
+	//模拟系统时间前进：以计算机的10s作为系统的1h
+	static void forward(TimeTable* table);
+
+	//课程闹钟：每晚9点提醒学生第二天上的课
+	static void remind_course(TimeTable* table);
 
 public:
 
